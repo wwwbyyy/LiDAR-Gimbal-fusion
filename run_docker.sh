745008294deec5be138b679079190f1fb3dd1c49
wuxinao <wuxinao5257@163.com>
2025-04-24 01:32:56 +0800

    fix: add user instruments

--- run_docker.sh
+++ run_docker.sh
@@ -2,4 +2,4 @@
 
 xhost + # for rviz
 
-docker run -it --net=host --restart=always -e DISPLAY=$DISPLAY --privileged -v /tmp/.X11-unix:/tmp/.X11-unix -v [YOUR PATH]:/home/loc/lidar_gimbal_loc/ --name lidar_gimbal_loc_container lidar_gimbal_loc
+docker run -it --net=host --restart=always -e DISPLAY=$DISPLAY --privileged -v /tmp/.X11-unix:/tmp/.X11-unix -v [YOUR catkin_ws path that contains /src]:/home/loc/loc_ws/ --name lidar_gimbal_loc_container lidar_gimbal_loc
